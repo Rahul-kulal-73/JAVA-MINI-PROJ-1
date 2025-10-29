643da5aee83743555fb07a773f0337825731ad80
Rahul Kulal <rahulkulala7@gmail.com>
2025-10-29 11:41:00 +0530

    Update UserDAO.java

--- src/main/java/com/yourdomain/todo/dao/UserDAO.java
+++ src/main/java/com/yourdomain/todo/dao/UserDAO.java
@@ -2,12 +2,12 @@ package com.yourdomain.todo.dao;
 
 import com.yourdomain.todo.model.User;
 import com.yourdomain.todo.util.DBConnection;
-import at.favre.lib.crypto.bcrypt.BCrypt; // Password hashing library
+import at.favre.lib.crypto.bcrypt.BCrypt; 
 import java.sql.*;
 
 public class UserDAO {
     private static final String INSERT_USER = "INSERT INTO users (username, password) VALUES (?, ?)";
-    private static final String SELECT_USER_BY_USERNAME = "SELECT * FROM users WHERE username = ?";
+    private static final String SELECT_USER_BY_USERNAME = "SELECT user_id, username, password FROM users WHERE username = ?";
 
     public boolean registerUser(User user) throws SQLException {
         // HASH the password before storing!
@@ -18,6 +18,9 @@ public class UserDAO {
             ps.setString(1, user.getUsername());
             ps.setString(2, hashedPassword);
             return ps.executeUpdate() > 0;
+        } catch (SQLIntegrityConstraintViolationException e) {
+             // Catch duplicate entry error (e.g., username already exists)
+             return false;
         }
     }
 
@@ -29,6 +32,7 @@ public class UserDAO {
             try (ResultSet rs = ps.executeQuery()) {
                 if (rs.next()) {
                     String storedHash = rs.getString("password");
+                    
                     // Verify password against stored hash
                     BCrypt.Result result = BCrypt.verifyer().verify(password.toCharArray(), storedHash.toCharArray());
                     
@@ -36,7 +40,7 @@ public class UserDAO {
                         user = new User();
                         user.setId(rs.getInt("user_id"));
                         user.setUsername(rs.getString("username"));
-                        // Do NOT set the password field on the returned User object for security
+                        // Do NOT copy the hash into the user object
                     }
                 }
             }
