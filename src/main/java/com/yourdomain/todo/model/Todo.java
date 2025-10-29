afd4c6d5b63038e39741a641af30ee69791236f2
Rahul Kulal <rahulkulala7@gmail.com>
2025-10-29 11:43:22 +0530

    Update Todo.java

--- src/main/java/com/yourdomain/todo/model/Todo.java
+++ src/main/java/com/yourdomain/todo/model/Todo.java
@@ -6,20 +6,46 @@ public class Todo {
     private int id;
     private int userId;
     private String title;
-    // ... all other fields (description, targetDate, isDone)
-    
-    // Constructors (omitted for brevity)
-    
-    // Getters and Setters (omitted for brevity)
-    
-    public String getTitle() { return title; }
-    public void setTitle(String title) { this.title = title; }
-    
+    private String description;
+    private LocalDate targetDate;
+    private boolean isDone;
+
+    // Default Constructor (REQUIRED)
+    public Todo() {}
+
+    // Constructor for creating new Todos
+    public Todo(int userId, String title, String description, LocalDate targetDate, boolean isDone) {
+        this.userId = userId;
+        this.title = title;
+        this.description = description;
+        this.targetDate = targetDate;
+        this.isDone = isDone;
+    }
+
+    // Constructor for fetching existing Todos
+    public Todo(int id, int userId, String title, String description, LocalDate targetDate, boolean isDone) {
+        this(userId, title, description, targetDate, isDone);
+        this.id = id;
+    }
+
+    // --- Getters and Setters (The methods TodoDAO is looking for) ---
+
     public int getId() { return id; }
     public void setId(int id) { this.id = id; }
     
     public int getUserId() { return userId; }
     public void setUserId(int userId) { this.userId = userId; }
     
-    // ... complete with all methods ...
+    public String getTitle() { return title; }
+    public void setTitle(String title) { this.title = title; }
+    
+    public String getDescription() { return description; }
+    public void setDescription(String description) { this.description = description; }
+    
+    public LocalDate getTargetDate() { return targetDate; }
+    public void setTargetDate(LocalDate targetDate) { this.targetDate = targetDate; }
+    
+    // Standard boolean getter is 'isDone()'
+    public boolean isDone() { return isDone; } 
+    public void setDone(boolean done) { isDone = done; }
 }
